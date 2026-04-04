d75a4f2d5eccfd6a877fafbfb1e03b423af8776b
Octavio Selis <octavioselis12@gmail.com>
2026-04-04 14:55:48 -0300

    Segundo commit del archivo tp1_1.c

--- tp1_1.c
+++ tp1_1.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 int main(){
-    printf("Hola Mundo");
+    printf("Hola Mundo\n");
+    int *puntero, apuntado = 1;
+    puntero = &apuntado;
+
+    printf("Contenido del puntero: %d\n", *puntero);
+    printf("Direccion de memoria almacenada por el puntero: %p\n", puntero);
+    printf("Direccion de memoria de la variable: %p\n", &apuntado);
+    printf("Direccion de memoria del puntero: %p\n", &puntero);
+    
+    int tamanio = sizeof(apuntado);
+
+    printf("Tamano de memoria usado por apuntado: %d bytes\n", tamanio);
+
 return 0;
 }
